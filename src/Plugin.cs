a9eccae917a57f408d7b5d2050697b82ecc22874
r0ute <route-a@ya.ru>
2026-07-28 03:06:04 -0500

    init project

---
+++ src/Plugin.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.Mono;
+using BusinessLayoutSets;
+using Entities;
+using HarmonyLib;
+
+namespace BA.src;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+
+    internal static new ManualLogSource Logger;
+
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+
+
+        Harmony harmony = new(MyPluginInfo.PLUGIN_GUID);
+        harmony.PatchAll(typeof(Patches));
+
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+
+    class Patches
+    {
+
+        [HarmonyPatch(typeof(WorkoutVarietyCustomerDemand), nameof(WorkoutVarietyCustomerDemand.Fulfilled))]
+        [HarmonyPostfix]
+        static void OnWorkoutVarietyCustomerDemandFulfilled(BuildingRegistration registration, HashSet<Item> items, ref bool __result)
+        {
+        }
+
+    }
+
+}
